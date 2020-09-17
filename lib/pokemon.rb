eb2cb85c3b4b559b63b588aa2fb464740b6b481c
Richard Kurwitz <kurwitz3@aol.com>
2020-09-17 20:58:44 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,4 +1,13 @@
 class Pokemon
   
-  attr_accessor :namr,
+  attr_accessor :name, :type
+  attr_reader :id, :db
+  
+  
+ def initialize(id=nil,name,type,db)
+   @id = id 
+   @name =name 
+   @type = type 
+   @db = db 
+ end 
 end
